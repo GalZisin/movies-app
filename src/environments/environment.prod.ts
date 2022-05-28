76f917722526b016b4549f68299b08fd5c28afaf
Gal Zisin <46171488+GalZisin@users.noreply.github.com>
2022-05-29 02:33:18 +0300

    remove api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://api.themoviedb.org/3',
-  apiKey: '3cbb3bfdacef8f68e64789aad1f00112',
+  apiKey: '',
   imageBaseUrl: 'https://image.tmdb.org/t/p'
 };
